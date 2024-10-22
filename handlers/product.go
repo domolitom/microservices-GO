e6302e9ef2072b7be604b0a65b99c5d3d91fc2fe
Oliver Domokos <domolitom@gmail.com>
2024-10-22 20:47:01 +0200

    add product handler which follows the rest api

---
+++ handlers/product.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"domolitom/microservices/data"
+	"log"
+	"net/http"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.GetProducts(rw, r)
+		return
+	}
+}
+
+func (p *Products) GetProducts(rw http.ResponseWriter, _ *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
