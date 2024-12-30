c055b41e15f2561b4139f1918af9fa695a633c22
Oliver Domokos <domolitom@gmail.com>
2024-12-30 14:51:49 +0200

    try gorilla

--- handlers/product.go
+++ handlers/product.go
@@ -4,11 +4,16 @@ import (
 	"domolitom/microservices/data"
 	"log"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Products struct {
 	l *log.Logger
 }
+
+type KeyProduct struct{}
 
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
@@ -26,5 +31,35 @@ func (p *Products) GetProducts(rw http.ResponseWriter, _ *http.Request) {
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+	data.AddProduct(&prod)
+}
+
+func (p Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(rw, "Unable to convert id", http.StatusBadRequest)
+		return
+	}
+
+	p.l.Println("Handle PUT Product", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
+
+	err = data.UpdateProduct(id, &prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusInternalServerError)
+		return
 	}
 }
