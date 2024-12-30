62ec6bc87e54f890ff1055ade9a8ef300ddfe159
Oliver Domokos <domolitom@gmail.com>
2024-12-30 14:55:53 +0200

    add more product methods

--- data/product.go
+++ data/product.go
@@ -2,11 +2,13 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
 
 type Product struct {
+	ID          int     `json:"id"`
 	Name        string  `json:"name"`
 	Description string  `json:"description"`
 	Price       float32 `json:"price"`
@@ -30,6 +32,7 @@ func AddProduct(p *Product) {
 
 var productList = Products{
 	&Product{
+		ID:          1,
 		Name:        "Guitar",
 		Description: "Furch Orange Guitar",
 		Price:       2000,
@@ -38,6 +41,7 @@ var productList = Products{
 		UpdatedOn:   time.Now().UTC().String(),
 	},
 	&Product{
+		ID:          2,
 		Name:        "Drums",
 		Description: "Yamaha Drums",
 		Price:       1500,
@@ -46,6 +50,32 @@ var productList = Products{
 		UpdatedOn:   time.Now().UTC().String(),
 	},
 }
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func findProduct(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
+}
+
+// func getNextID() int {
+// 	lp := productList[len(productList)-1]
+// 	return lp.ID + 1
+// }
 
 func (p *Products) ToJSON(w io.Writer) error {
 	e := json.NewEncoder(w)
